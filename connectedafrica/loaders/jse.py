0575dd8eb1a715da3043b03edfa4e2e195db6d29
Friedrich Lindenberg <friedrich@pudo.org>
2014-06-17 17:14:53 +0300

    Separate scraping and loading parts, multi-thread scraper.

--- connectedafrica/loaders/jse.py
+++ connectedafrica/loaders/jse.py
@@ -1,16 +1,21 @@
 import logging
 from pprint import pprint
+#from threading import RLock
 import json
 from itertools import count
 
+from thready import threaded
 from granoclient.loader import Loader
 import requests
 
 from connectedafrica.core import grano
+from connectedafrica.loaders.util import write_json, read_json
 
 
 log = logging.getLogger(__name__)
+#scrape_lock = RLock()
 
+INDEX = 'https://www.jse.co.za/_vti_bin/JSE/CustomerRoleService.svc/GetAllIssuers'
 ISSUER = 'https://www.jse.co.za/_vti_bin/JSE/CustomerRoleService.svc/GetIssuer'
 BUSINESS = 'https://www.jse.co.za/_vti_bin/JSE/CustomerRoleService.svc/GetIssuerNatureOfBusiness'
 ASSOCIATED = 'https://www.jse.co.za/_vti_bin/JSE/CustomerRoleService.svc/GetIssuerAssociatedRoles'
@@ -40,10 +45,32 @@ def create_entity(loader, record):
     return ent
 
 
-def load_record(i):
+def load_record(i, record):
+    """ Load an individual record to grano. """
     source_url = SOURCE_URL % i
     loader = Loader(grano, source_url=source_url)
 
+    log.info('Loading: %s', record.get('LongName'))
+    ent = create_entity(loader, record)
+
+    for role in record.pop('AssociatedRoles'):
+        role_desc = role.get('RoleDescription')
+        print role_desc
+        alt = create_entity(loader, role)
+
+        rel = loader.make_relation('jse_role', alt, ent)
+        rel.set('role', role_desc)
+        rel.save()
+
+
+def load():
+    records = read_json('jse.json') or {}
+    for i, record in records.items():
+        load_record(i, record)
+
+
+def scrape_record(records, i):
+    """ Scrape data regarding a single listed company. """
     req = {
         'data': json.dumps({'issuerMasterId': i}),
         'headers': {'Content-Type': 'application/json'}
@@ -51,30 +78,41 @@ def load_record(i):
     res = requests.post(ISSUER, **req)
     if res.status_code != 200 or res.json().get('GetIssuerResult') is None:
         return
-    record = res.json().get('GetIssuerResult')
-    res = requests.post(BUSINESS, **req)
-    record['NatureOfBusiness'] = res.json().get('GetIssuerNatureOfBusinessResult')
     
-    log.info('Loading: %s', record.get('LongName'))
-    ent = create_entity(loader, record)
+    record = res.json().get('GetIssuerResult')
+    log.info('Scraping: %s', record.get('LongName'))
+    
+    try:
+        res = requests.post(BUSINESS, **req)
+        record['NatureOfBusiness'] = res.json().get('GetIssuerNatureOfBusinessResult')
+    except Exception, e:
+        log.exception(e)
 
     res = requests.post(ASSOCIATED, **req)
-    for role in res.json().get('GetIssuerAssociatedRolesResult'):
-        role_desc = role.get('RoleDescription')
-        alt = create_entity(loader, role)
+    record['AssociatedRoles'] = res.json().get('GetIssuerAssociatedRolesResult')
 
-        rel = loader.make_relation('jse_role', alt, ent)
-        rel.set('role', role_desc)
-        rel.save()
-
-    #res = requests.post(SENS, **req)
-    #pprint(record)
+    records[i] = record
 
 
-def load():
-    for i in count(1):
-        load_record(i)
+def scrape_index(records):
+    """ Get a list of listed companies. """
+    req = {
+        'data': json.dumps({}),
+        'headers': {'Content-Type': 'application/json'}
+    }
+    res = requests.post(INDEX, **req)
+    for row in res.json():
+        master_id = str(row.get('MasterID'))
+        if master_id not in records:
+            yield master_id
+
+
+def scrape():
+    records = read_json('jse.json') or {}
+    threaded(scrape_index(records), lambda i: scrape_record(records, i), num_threads=30)
+    write_json('jse.json', records)
 
 
 if __name__ == '__main__':
+    scrape()
     load()
