12dd345ddb73454a2e85496371293a679e0538d7
Friedrich Lindenberg <friedrich@pudo.org>
2014-07-21 18:21:16 +0200

    A lot of work on static pages.

--- connectedafrica/views/base.py
+++ connectedafrica/views/base.py
@@ -14,12 +14,17 @@ def index():
         'facet': 'schema'
     }
     s, results = grano.client.get('/entities', params=params)
-    return render_template('index.html',
+    features = [p for p in pages if p.meta.get('featured')]
+    return render_template('index.html', features=features,
                            schemata=results.get('facets').get('schema'))
 
 
 @blueprint.route('/pages/<path:path>/')
 def page(path):
     page = pages.get_or_404(path)
+    menu = [p for p in pages if p.meta.get('menutitle')]
+    menu = sorted(menu, key=lambda p: p.meta.get('menuindex'))
+    children = [p for p in pages if p.meta.get('parent') == page.path]
     template = page.meta.get('template', 'pages.html')
-    return render_template(template, page=page)
+    return render_template(template, page=page, menu=menu,
+                           children=children)
---
+++ pages/stories/zuma.md
@@ -0,0 +1,10 @@
+title: All the President's Men
+parent: stories
+featured: true
+teaser: >
+  We've mapped out the holdings of President Jacob Zuma and his
+  immediate family, to find the ties that they hold to industry
+  inside and outside South Africa.
+published: 2014-07-03
+
+I'm a banana!
