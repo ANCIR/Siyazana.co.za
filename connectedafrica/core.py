892ad5f35bbbd6a20d2fd87b48eee51fcc6cfffd
Friedrich Lindenberg <friedrich@pudo.org>
2014-08-02 20:09:36 +0200

    Minor changes to deployment.

--- connectedafrica/core.py
+++ connectedafrica/core.py
@@ -14,7 +14,6 @@ app.config.from_envvar('COAF_SETTINGS', silent=True)
 assets = Environment(app)
 pages = FlatPages(app)
 
-
 # Set up an API client for the grano instance:
 
 client = Grano(api_host=app.config.get('GRANO_HOST'),
@@ -24,8 +23,7 @@ project_name = app.config.get('GRANO_PROJECT')
 try:
     grano = client.get(project_name)
 except NotFound:
-    data = {'slug': project_name, 'label': project_name}
-    grano = client.projects.create(data)
+    raise RuntimeError("Please run 'make loadschema' first!")
 
 
 class SchemaCache(object):
